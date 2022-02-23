efa976ad91fa66f3a6f28c84418262b1b5bbad03
teruru2017 <teruru2012r@gmail.com>
2022-02-24 02:02:07 +0700

    24/2/22

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,37 +7,38 @@ const Footer = () => {
       <footer className='mt-3 py-2'>
         <ul className='nav justify-content-center border-top'>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
-            <small><small>OCTA</small></small>
+            <a href='#' className='nav-link px-2 text-white'>
+            <img src="/logo.png" alt="" height={20}/>
+            
             </a>
           </li>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='#' className='nav-link px-2 text-white'>
               <FaFacebook />
             </a>
           </li>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='#' className='nav-link px-2 text-white'>
             <FaTwitter />
             </a>
           </li>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='#' className='nav-link px-2 text-white'>
             <FaInstagram />
             </a>
           </li>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='#' className='nav-link px-2 text-white'>
             <FaDiscord />
             </a>
           </li>
           <li className='nav-item'>
-            <a href='#' className='nav-link px-2 text-muted'>
+            <a href='#' className='nav-link px-2 text-white'>
             <small><small>Contact us</small></small>
             </a>
           </li>
         </ul>
-        <p className='text-center text-muted'><small><small>Copyright © 2021 OCTA Web, Inc</small></small></p>
+        <p className='text-center text-white'><small><small>Copyright © 2021 OCTA Web, Inc</small></small></p>
         <LoadSpinner />
       </footer>
     </div>
